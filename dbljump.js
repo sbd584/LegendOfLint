c898acc5b7631a89a649927d6c72cd4bbd3dbc85
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-12-08 14:36:10 -0600

    Add files via upload

---
+++ dbljump.js
@@ -0,0 +1,34 @@
+demo.dbljump = function (){};
+demo.dbljump.prototype = {
+    preload: function (){
+        game.load.image('pwrUp', 'lintAssets/shoe_powerup_screen.png');
+        game.load.image('howto', 'howto_doublejump.png');
+    },
+    create: function (){
+
+        current_lvl = 0;
+        console.log('double jump');
+
+        game.stage.backgroundColor = '#000000';
+
+        game.world.setBounds(0, 0, 1200, 800);
+        pwr = game.add.sprite(0, 0, 'pwrUp');
+        pwr.visible = true;
+        inst = game.add.sprite(0, 0, 'howto');
+        inst.visible = false;
+
+    },
+    update: function (){
+        game.time.events.add(Phaser.Timer.SECOND * 5, function(){
+            pwr.visible = false;
+            inst.visible = true;
+        })
+        game.time.events.add(Phaser.Timer.SECOND * 10, function(){
+            dbl();
+        })
+    }
+}
+
+function dbl(){
+    game.state.start('lvl3');
+}
