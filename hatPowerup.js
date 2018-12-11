7435a52aaa7ab9b4e95d5837643d5c8930cdca17
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-12-10 23:19:49 -0600

    Add files via upload

--- hatPowerup.js
+++ hatPowerup.js
@@ -2,6 +2,7 @@ demo.hatPowerup = function (){};
 demo.hatPowerup.prototype = {
     preload: function (){
         game.load.image('hat', 'lintAssets/endscene/howto_irresistible.png');
+        game.load.image('hat2', 'lintAssets/hat_pup_screen.png');
     },
     create: function (){
 
@@ -11,12 +12,18 @@ demo.hatPowerup.prototype = {
         game.stage.backgroundColor = '#000000';
 
         game.world.setBounds(0, 0, 1200, 800);
-        pwr = game.add.sprite(0, 0, 'hat');
-        pwr.visible = true;
+        pwr1 = game.add.sprite(0, 0, 'hat2');
+        pwr1.visible = true;
+        pwr2 = game.add.sprite(0, 0, 'hat');
+        pwr2.visible = false;
 
     },
     update: function (){
         game.time.events.add(Phaser.Timer.SECOND * 5, function(){
+            pwr1.visible = false;
+            pwr2.visible = true;
+        })
+        game.time.events.add(Phaser.Timer.SECOND * 10, function(){
             game.state.start('getTheGirl');
         })
     }
